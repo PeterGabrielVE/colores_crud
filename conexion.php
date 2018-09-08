7abe274efe4b79635c318e72cbe3d0c31661532d
PeterGabrielVE <gaboleal123@gmail.com>
2018-09-07 19:40:25 -0430

    Conexion PDO
    creando conexion

---
+++ conexion.php
@@ -0,0 +1,22 @@
+<?php
+
+	$link = 'mysql:host=localhost;dbname=yt_colores';
+	$usuario = 'root';
+	$pass = '';
+
+	try{
+
+		$pdo = new PDO($link,$usuario,$pass);
+
+			foreach($pdo->query('SELECT * from `colores`') as $fila) {
+        	print_r($fila);
+    		}
+    	
+		echo 'Conectado';
+
+	}catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
